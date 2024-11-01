238eb291a26c8763409b2ee46615d8494a372437
Ayush Singh ☁️ <ayush1337@hotmail.com>
2024-11-01 15:18:55 +0530

    Add skipValidation option to environment configuration

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -2,6 +2,8 @@ import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
 export const env = createEnv({
+  skipValidation: true,
+  
   server: {
     NODE_ENV: z
       .enum(["development", "test", "production"])
