c09a25b3dc31b2b8c8af35024341252408531d09
ayush-that <ayush1337@hotmail.com>
2024-12-18 00:32:07 +0530

    Add resume

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,33 @@
-import {build} from 'velite'
+import { build } from "velite";
 
-import {fileURLToPath} from 'node:url'
-import createJiti from 'jiti'
-const jiti = createJiti(fileURLToPath(import.meta.url))
+import { fileURLToPath } from "node:url";
+import createJiti from "jiti";
+const jiti = createJiti(fileURLToPath(import.meta.url));
 
-jiti('./src/constants/env')
+jiti("./src/constants/env");
 
-// Note that this approach uses top-level await, so it only supports next.config.mjs or ESM enabled.
-const isDev = process.argv.indexOf('dev') !== -1
-const isBuild = process.argv.indexOf('build') !== -1
+const isDev = process.argv.indexOf("dev") !== -1;
+const isBuild = process.argv.indexOf("build") !== -1;
 
 if (!process.env.VELITE_STARTED && (isDev || isBuild)) {
-  process.env.VELITE_STARTED = '1'
-  const {build} = await import('velite')
-  await build({watch: isDev, clean: !isDev})
+  process.env.VELITE_STARTED = "1";
+  const { build } = await import("velite");
+  await build({ watch: isDev, clean: !isDev });
 }
 
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {}
+const nextConfig = {
+  redirects: async () => {
+    return [
+      {
+        source: "/resume",
+        destination:
+          "https://drive.google.com/file/d/1hjYXa-4jvB7SmEN7CsopiI_hX4fn0eVq/view",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-export default nextConfig
+export default nextConfig;
