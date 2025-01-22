18a7ef1a8b9f48b37a48281fe880fdcc348c2fda
ayush-that <ayush1337@hotmail.com>
2025-01-23 00:36:00 +0530

    removed dead resume link

--- src/components/layout/nav/_nav-mock.ts
+++ src/components/layout/nav/_nav-mock.ts
@@ -16,23 +16,6 @@ export const navData = [
     label: "Blog",
     path: "/blog",
   },
-  {
-    id: createId(),
-    label: "Resume",
-    path: "https://drive.google.com/file/d/1hjYXa-4jvB7SmEN7CsopiI_hX4fn0eVq/view?usp=sharing",
-  },
-
-  // {
-  //   id: createId(),
-  //   label: 'Guests',
-  //   path: '/guests',
-  // },
-
-  // {
-  //   id: createId(),
-  //   label: 'Contact',
-  //   path: '/contact',
-  // },
 ];
 
 export type NavType = typeof navData;
