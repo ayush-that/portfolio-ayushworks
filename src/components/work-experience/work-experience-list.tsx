a41cf008dfdbc4a94b89c7baca0e7f18b1b06f52
ayush-that <ayush1337@hotmail.com>
2026-02-20 23:06:31 +0530

    feat: add API for fetching page views and improve code formatting

--- src/components/work-experience/work-experience-list.tsx
+++ src/components/work-experience/work-experience-list.tsx
@@ -7,15 +7,10 @@ type WorkExperienceListProps = {
   showTitle?: boolean;
 };
 
-const WorkExperienceList = ({
-  experiences,
-  showTitle = true,
-}: WorkExperienceListProps) => {
+const WorkExperienceList = ({ experiences, showTitle = true }: WorkExperienceListProps) => {
   return (
     <section aria-label="work experience" className="mt-5 space-y-6">
-      {showTitle && (
-        <h2 className={typo({ variant: "h2" })}>Work Experience</h2>
-      )}
+      {showTitle && <h2 className={typo({ variant: "h2" })}>Work Experience</h2>}
 
       <div className="!mt-8">
         <ol className="space-y-6" role="list">
